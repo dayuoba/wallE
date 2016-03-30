8fb2b300d7274e56ae090aad5ca00b4001b5e640
yuyang <yuyang@goyoo.com>
2016-03-30 18:03:46 +0800

    first commit

---
+++ wallE.js
@@ -0,0 +1,37 @@
++function wallE(window) {
+    var we = {};
+
+    we.collect = function(data, type) {
+      // push data to sets
+      // if fullfill sets,cancle the event listeners
+    };
+
+    we.init = function() {
+      //register events
+    };
+
+    // utils
+    // zip
+    function pakmsg(obj) {
+      var str = obj.toString();
+      // a zip algorithm
+      return str;
+    }
+
+    // ajax
+    function send(msg, cb) {
+      //call ajax send
+      //cb(ajax.resp)
+    }
+
+    function clickHandler() {
+
+    }
+
+    function moveHandler() {
+    }
+
+    function cookieParser() {
+    }
+
+  }(window)
